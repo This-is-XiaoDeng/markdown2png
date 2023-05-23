f3e25b04a193dacd84419ddd76cc5d405c39b117
This is XiaoDeng <1744793737@qq.com>
2023-05-23 20:51:11 +0800

    修复无法正常渲染代码块换行的问题

--- markdown2png/parser.py
+++ markdown2png/parser.py
@@ -3,18 +3,20 @@ import marko
 
 def markdown2html(markdown: str) -> str:
     print(marko.convert(markdown))
-    return f"<div>{marko.convert(markdown)}</div>".replace("\n\n", "<br />").replace("\n", "")
+    return f"<div>{marko.convert(markdown)}</div>".replace("\n\n", "<br />")
 
-def parse_dom(nodes: list) -> list:
+def parse_dom(nodes: list, is_code: bool = False) -> list:
     ast, item = [], {}
     for node in nodes:
         item["type"] = node.nodeName
         if node.attributes != None:
             for attr in node.attributes.items():
                 item[attr[0]] = attr[1]
-        item["innerHTML"] = parse_dom(node.childNodes)
+        item["innerHTML"] = parse_dom(node.childNodes, item.get("type") == "code")
         if node.nodeType == node.TEXT_NODE:
             item = node.data
+            if not is_code:
+                item = item.replace("\n", "")
         ast.append(item)
         item = {}
     return ast
--- markdown2png/style.py
+++ markdown2png/style.py
@@ -25,22 +25,34 @@ class DefaultStyle(Style):
             self.widghts[f"h{i}"] = eval(f"lambda data: head({i}, data)")
 
 def code(data: dict) -> Surface:
-    # print(data)
-    text = pygame.font.Font(
-        os.path.join(path, "font/sarasa-fixed-cl-regular.ttf"), 20).render(
-            "".join(data["innerHTML"]), True, (255, 0, 51), (204, 204, 204))
     if "language-" in str(data.get("class")):
         _language = data["class"].replace("language-", "")
     else:
         _language = None
-    surface = Surface((text.get_size()[0], text.get_size()[1] if not _language else (text.get_size()[1] + 25)))
+    
+    if not _language:
+        text = pygame.font.Font(
+            os.path.join(path, "font/sarasa-fixed-cl-regular.ttf"), 20).render(
+                "".join(data["innerHTML"]), True, (255, 0, 51), (204, 204, 204))
+        surface = Surface((text.get_size()[0], text.get_size()[1]))
+    else:
+        data["innerHTML"] = "".join(data["innerHTML"])
+        font = pygame.font.Font(os.path.join(path, "font/sarasa-fixed-cl-regular.ttf"), 20)
+        max_width = 0
+        for line in data["innerHTML"].splitlines():
+            max_width = max(max_width, font.render(line, True, (0, 0, 0)).get_size()[0])
+        surface = Surface((max_width, len(data["innerHTML"].splitlines()) * 20 + 30))
     surface.fill(BG_COLOR if not _language else (204, 204, 204))
+
     if _language:
         language = pygame.font.Font(
             os.path.join(path, "font/sarasa-fixed-cl-regular.ttf"), 20).render(
                 _language, True, (255, 0 ,51), (204, 204, 204))
         surface.blit(language, (0, 0))
-        surface.blit(text, (0, 25))
+        y = 25
+        for line in data["innerHTML"].splitlines():
+            surface.blit(font.render(line, True, (255, 0, 51), (204, 204, 204)), (0, y))
+            y += 20
     else:
         surface.blit(text, (0, 0))
     return surface
